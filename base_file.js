ee352cbea133b265a65ab23c776f7a6d16c7e185
Azarudeen <azarudeendanish@gmail.com>
2022-02-20 11:25:17 +0400

    beta api change

--- base_file.js
+++ base_file.js
@@ -1,2 +1,3 @@
 // export const BASE_URL = "https://api.homegenie.com/api/";
+// export const BASE_URL = "https://dev.api.homegenie.com/api/";
 export const BASE_URL = "https://beta.api.homegenie.com/api/";
